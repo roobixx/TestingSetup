e2d2e52d475634e02f2c60fe4af9acf48e86bb30
Tim Fowler <tim@roobixx.com>
2016-10-17 14:28:28 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-## Update and install required packages
-
 sudo apt-get update --assume-yes
-sudo apt-get dist-upgrade --assume-yes
-sudo apt-get install openvpn nmap firefox xauth xorg git --assume-yes
+sudo apt-get install openvpn nmap firefox xauth xorg git rdesktop smbclient samba --assume-yes
 
 
 ## Install EyeWitnesss
 
 git clone https://github.com/ChrisTruncer/EyeWitness.git
 cd EyeWitness/setup
-sudo ./setup.sh
+sudo ./setup
 cd 
+
+# Install Responder
+
+git clone https://github.com/lgandx/Responder.git
 
 ## Install Metaploit	
 
 wget http://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run
-sudo chmod +x metasploit-latest-linux-x64-installer.run
 sudo ./metasploit-latest-linux-x64-installer.run
+
+## Configure VPN
+
+#echo -n "What is the name of the user you wish to configure for the VPN?: "
+#read user
+
+#curl -u $user -k https://96.35.208.218:943/rest/GetUserlogin > /etc/openvpn/$user.conf
+
